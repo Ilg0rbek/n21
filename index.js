adb87d7c9097da6a8cf1d0957913d5d36a015ab6
ilg0rbek <ilgorbekabdumalikov331@gmail.com>
2025-03-21 17:54:17 +0500

    done homework

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("N21");
+
+// 1. { a: 10, b: 20, c: "hello", d: 30 } object berilgan uning raqamlar yig'indisini hisoblang
+// 2. { Ali: 75, Bekzod: 88, Diyor: 92, Fayz: 85 } berilgan objectdagi eng katta yoshni toping
+// 3. { name: "Ali", age: 25, country: "Uzbekistan" } objectning valuelarini keylar bilan alishtiring
+// 4. { name: "Aziz", city: "Tashkent", age: 25, country: "Uzbekistan" } objectning har bir valuesni uzunligini hisoblang
+
